82d810e4130abb2e5ece9fdfcc124c1bb7afccc7
Eddh <jegghel@gmail.com>
2015-01-09 12:49:44 +0100

    VictoryPanelfinal

--- small_UTBM_world/src/view/VictoryPanel.java
+++ small_UTBM_world/src/view/VictoryPanel.java
@@ -2,10 +2,13 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import controller.BackButtonListener;
 import model.Player;
 
 public class VictoryPanel extends JPanel {
@@ -15,13 +18,20 @@ public class VictoryPanel extends JPanel {
 
 	private PlayerPanel pp;
 	private JLabel message;
+	private JButton restartBtn;
+	private BackButtonListener bbl;
 	public VictoryPanel(Player p){
+		setLayout(new FlowLayout());
+		restartBtn=new JButton("Recommencer");
+		bbl=new BackButtonListener(GlobalPanel.getInstance());
 		message= new JLabel("Le joueur " + p.getName() + " a remporté la victoire");
 		pp= new PlayerPanel(p);
-		add(message, BorderLayout.PAGE_START);
-		add(pp,BorderLayout.PAGE_END);
-		
+		add(message);
+		add(pp);
+		add(restartBtn);
+		restartBtn.addMouseListener(bbl);
 		pp.setVisible(true);
 		message.setVisible(true);
+		restartBtn.setVisible(true);
 	}
 }
